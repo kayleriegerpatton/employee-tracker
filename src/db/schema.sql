306c3871ccd2cc42abda8118b8ca299aecc34314
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-12-05 14:53:24 +0000

    database schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -15,10 +15,23 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL(10,2) CHECK (salary > 0),
 department_id INT,
-
+-- roles.department_id foreign key relates to departments.id primary key
 FOREIGN KEY (department_id) REFERENCES departments(id) 
 ON UPDATE CASCADE 
 ON DELETE SET NULL
 );
 
 -- Employees table
+CREATE TABLE employees (
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT,
+
+FOREIGN KEY (role_id) REFERENCES role(id)
+ON UPDATE CASCADE 
+ON DELETE SET NULL
+
+
+);
