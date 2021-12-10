3fe2457b7de43ae537c7831ffd6a9eb41d1a0f4f
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-12-10 16:58:17 +0000

    update queries to match criteria

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,16 +1,23 @@
-const allEmployeesQuery = `SELECT employee_role.first_name AS "First Name", employee_role.last_name AS "Last Name", title AS "Role", salary AS "Salary", name AS "Department", CONCAT (employee_manager.first_name, " ", employee_manager.last_name) as "Manager" FROM employee employee_role
+const allEmployeesQuery = `SELECT employee_role.first_name AS "First Name", employee_role.last_name AS "Last Name", employee_role.id AS "ID", title AS "Role", name AS "Department", salary AS "Salary", CONCAT (employee_manager.first_name, " ", employee_manager.last_name) as "Manager" FROM employee employee_role
 LEFT JOIN role ON employee_role.role_id = role.id
 LEFT JOIN department ON role.department_id = department.id
 LEFT JOIN employee employee_manager ON employee_role.manager_id = employee_manager.id;`;
 
 // view all roles
-const allRolesQuery = `SELECT role.title AS Role, role.salary AS Salary, department.name AS Department FROM role
+const allRolesQuery = `SELECT role.title AS Role, role.salary AS Salary, department.name AS Department, role.id AS ID FROM role
   JOIN department ON role.department_id = department.id
   ORDER BY department.name;`;
 
 // view all depts
-const allDepartmentsQuery = `SELECT department.name AS Departments FROM department
+const allDepartmentsQuery = `SELECT department.id AS ID, department.name AS Departments FROM department
   ORDER BY name;`;
+
+// view employees by manager
+const employeesByManager = `SELECT employee_role.first_name as "First Name", employee_role.last_name as "Last Name", title as "Role", salary as "Salary", name as "Department", CONCAT (employee_manager.first_name, " ", employee_manager.last_name) as "Manager" 
+FROM employee employee_role
+LEFT JOIN role ON employee_role.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id
+LEFT JOIN employee employee_manager ON employee_role.manager_id = employee_manager.id WHERE employee_role.manager_id = 3;`;
 
 module.exports = {
   allEmployeesQuery,
